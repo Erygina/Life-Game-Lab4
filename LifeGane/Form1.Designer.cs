92da51e412bab33623f6f45d4d4aef55485aee22
Erygina <viktoriya_erygina@inbox.ru>
2023-03-08 14:15:11 +0700

    Добавьте файлы проекта.

---
+++ LifeGane/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace LifeGane
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.newBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.startBtn);
+            this.panel1.Controls.Add(this.newBtn);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(28, 83);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(98, 51);
+            this.startBtn.TabIndex = 2;
+            this.startBtn.Text = "Старт";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // newBtn
+            // 
+            this.newBtn.Location = new System.Drawing.Point(28, 12);
+            this.newBtn.Name = "newBtn";
+            this.newBtn.Size = new System.Drawing.Size(98, 51);
+            this.newBtn.TabIndex = 1;
+            this.newBtn.Text = "Новая карта";
+            this.newBtn.UseVisualStyleBackColor = true;
+            this.newBtn.Click += new System.EventHandler(this.newBtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(200, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 450);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button newBtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button startBtn;
+    }
+}
+
---
+++ LifeGane/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LifeGane
+{
+    public partial class Form1 : Form
+    {
+        bool[,] field;
+
+        Bitmap bmp;
+        Graphics graphics;
+        Random rand = new Random();
+        Pen pen = new Pen(Color.White, 1);
+        int m, n, scale = 7; 
+
+        public Form1()
+        {
+            InitializeComponent();
+            m = pictureBox1.Height;
+            n = pictureBox1.Width;
+            field = new bool[m, n];
+
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(bmp);
+        }
+        void create()
+        {
+            for (int i = 0; i < m; i++)    //по бокам неживые клетки
+            {
+                field[i, 0] = false;
+                field[i, n - 1] = false;
+            }
+            for (int j = 0; j < n; j++)
+            {
+                field[0, j] = false;
+                field[m - 1, j] = false;
+            }
+
+            for (int i = 1; i < m - 1; i++)    //случайно определяются на карте живые клетки
+            {
+                for (int j = 1; j < n - 1; j++)
+                {
+                    if (rand.Next(0, 4) >= 3)
+                    {
+                        field[i, j] = true;
+                    }
+                    else
+                    {
+                        field[i, j] = false;
+                    }
+                }
+            }
+        }
+        private void render()
+        {
+
+            graphics = Graphics.FromImage(bmp);
+
+            graphics.Clear(Color.Black);
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)   //прорисовка на карте
+                {
+                    if (field[i, j])
+                    {
+                        graphics.DrawRectangle(pen, i * scale, j * scale, scale, scale);
+                    }
+                }
+            }
+
+            pictureBox1.Image = bmp;
+
+        }
+
+        private int neighbours(int i, int j) 
+        {
+            int neigh = 0;
+            if (field[i, j + 1])
+            {
+                neigh++;
+            }
+            if (field[i + 1, j + 1])
+            {
+                neigh++;
+            }
+            if (field[i + 1, j])
+            {
+                neigh++;
+            }
+            if (field[i + 1, j - 1])
+            {
+                neigh++;
+            }
+            if (field[i, j - 1])
+            {
+                neigh++;
+            }
+            if (field[i - 1, j - 1])
+            {
+                neigh++;
+            }
+            if (field[i - 1, j])
+            {
+                neigh++;
+            }
+            if (field[i - 1, j + 1])
+            {
+                neigh++;
+            }
+            return neigh;
+        }
+        private void step()
+        {
+
+            bool[,] next = new bool[m, n];
+            for(int i = 1; i < m - 1; i++)
+                for(int j = 1; j < n - 1; j++)
+                {
+                    int neigh = neighbours(i, j);
+                    if (field[i, j])
+                    {
+
+                        if (neigh >= 2 && neigh <= 3)
+                        {
+                            next[i, j] = true;
+                        }
+                        else
+                        {
+                            next[i, j] = false;
+                        }
+
+                    }
+                    else
+                    {
+                        if (neigh == 3)
+                        {
+                            next[i, j] = true;
+                        }
+                        else
+                        {
+                            next[i, j] = false;
+                        }
+                    }
+
+                }
+                field = next;
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void newBtn_Click(object sender, EventArgs e)
+        {
+            create();
+            render();
+            
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            step();
+            render();
+        }
+    }
+}
